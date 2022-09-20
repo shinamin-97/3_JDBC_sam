384106a4dd211ead0205688845de74a9049a571d
Shinamin_97 <qnrlgkals@naver.com>
2022-09-20 15:08:20 +0900

    Member_~

--- 회원제게시판.sql
+++ 회원제게시판.sql
@@ -43,11 +43,71 @@ INCREMENT BY 1
 NOCYCLE 
 NOCACHE;
 
+-- 회원 가입 INSERT
+INSERT INTO "MEMBER"
+VALUES( SEQ_MEMBER_NO.NEXTVAL, 'user01', 'pass01', '유저일', 'M', DEFAULT, DEFAULT);
+
+INSERT INTO "MEMBER"
+VALUES( SEQ_MEMBER_NO.NEXTVAL, 'user02', 'pass02', '유저이', 'F', DEFAULT, DEFAULT);
+
+INSERT INTO "MEMBER"
+VALUES( SEQ_MEMBER_NO.NEXTVAL, 'user03', 'pass03', '유저삼', 'F', DEFAULT, DEFAULT);
+
+SELECT * FROM "MEMBER";
+
+COMMIT;
 
 
+-- 아이디 중복 확인
+-- (중복되는 아이디가 입력되어도 탈퇴한 계정이면 중복X)
+SELECT COUNT(*) FROM "MEMBER"
+WHERE MEMBER_ID = 'user01'
+AND SECESSION_FL = 'N';
+--> ID가 user01 이면서 탈퇴하지 않은 회원 조회
 
+-- 중복면 1, 아니면 0 조회
+SELECT * FROM MEMBER;
 
+-- 로그인(아이디, 비밀번호 일치 + 탈퇴 X 회원)
+SELECT MEMBER_NO, MEMBER_ID, MEMBER_NM, MEMBER_GENDER,
+	TO_CHAR(ENROLL_DATE, 'YYYY"년" MM"월" DD"일" HH24:MI:SS') ENROLL_DATE
+FROM "MEMBER"
+WHERE MEMBER_ID = 'user01'
+AND MEMBER_PW = 'pass01'
+AND SECESSION_FL = 'N';
 
+-- 회원 목록 조회(아이디, 이름, 성별)
+-- 탈퇴 회원 미포함
+-- 가입일 내림차순
+	--> MEMBER_NO 내림차순 (나중에 가입한 회원의 번호가 더 큼)
+SELECT MEMBER_NO, MEMBER_ID, MEMBER_NM, MEMBER_GENDER
+FROM "MEMBER"
+WHERE SECESSION_FL = 'N' 
+ORDER BY MEMBER_NO DESC;
+
+-- 회원 정보 수정(이름, 성별)
+UPDATE "MEMBER" SET
+MEMBER_NM = '바꾼이름2', -- 입력
+MEMBER_GENDER = 'F' -- 입력
+WHERE MEMBER_NO = 1; -- loginMember.getMemberNo();
+
+SELECT * FROM MEMBER;
+
+ROLLBACK;
+
+-- 비밀번호 변경
+UPDATE "MEMBER" SET
+MEMBER_PW = '새비밀번호'
+WHERE MEMBER_NO = 1
+AND MEMBER_PW = '현재비밀번호';
+
+ROLLBACK;
+
+-- 회원 탈퇴(SECESSION_FN 컬럼의 값을 'Y'로 변경)
+UPDATE "MEMBER" SET
+SECESSION_FN = 'Y'
+WHERE MEMBER_NO = ?
+AND MEMBER_PW = ?
 
 
 
